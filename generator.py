a1178b2259b90b47b8ef50cf7a29661347876cb6
chrisaph <christianmerano26@gmail.com>
2026-06-03 12:17:39 +0000

    Initial RPG Character Generator

---
+++ generator.py
@@ -0,0 +1,8 @@
+# RPG Character Generator
+def main():
+print("--- Welcome to the Character Generator ---")
+# Components will be combined here
+name = "Hero"
+print(f"\nCharacter {name} has been successfully created!")
+if __name__ == "__main__":
+main()
