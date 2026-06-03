7aa57ff7289a886a7744a2d61429476f42815d27
chrisaph <christianmerano26@gmail.com>
2026-06-03 11:26:34 +0000

    Add attribute rolling feature

--- generator.py
+++ generator.py
@@ -1,4 +1,15 @@
 # RPG Character Generator
+
+import random
+
+def roll_attributes():
+    return {
+        "Strength": random.randint(5, 20),
+        "Agility": random.randint(5, 20),
+        "Intellect": random.randint(5, 20)
+    }
+
+
 def main():
  print("--- Welcome to the Character Generator ---")
  # Components will be combined here
